85d33662cb15fe6e35bc0f0d0482113894fa16b7
EderSantana <eder@macbook.com>
2015-07-10 11:42:40 -0400

    Added Convolutional sparse coding layer

--- seya/layers/coding.py
+++ seya/layers/coding.py
@@ -91,3 +91,66 @@ class SparseCoding(Layer):
                 "activation": self.activation.__name__,
                 "truncate_gradient": self.truncate_gradient,
                 "return_reconstruction": self.return_reconstruction}
+
+
+class ConvSparseCoding(Layer):
+    def __init__(self, nb_filter, stack_size, nb_row, nb_col,
+                 input_row, input_col,
+                 init='glorot_uniform', activation='linear', weights=None,
+                 border_mode='valid', subsample=(1, 1),
+                 W_regularizer=l2(.01), activity_regularizer=None):
+
+        super(ConvSparseCoding, self).__init__()
+        self.init = initializations.get(init)
+        self.activation = activations.get(activation)
+        self.subsample = subsample
+        self.border_mode = border_mode
+        self.nb_filter = nb_filter
+        self.stack_size = stack_size
+
+        self.nb_row = nb_row
+        self.nb_col = nb_col
+        if border_mode == 'valid':
+            self.code_row = input_row + nb_row - 1
+            self.code_col = input_col + nb_col - 1
+
+        self.input = T.tensor4()
+        self.W_shape = (nb_filter, stack_size, nb_row, nb_col)
+        self.W = self.init(self.W.shape)
+
+        self.params = [self.W]
+
+        self.regularizers = []
+        if W_regularizer:
+            W_regularizer.set_param(self.W)
+            self.regularizers.append(W_regularizer)
+
+        if weights is not None:
+            self.set_weights(weights)
+
+    def _step(self, x_t, accum_1, accum_2, inputs):
+        conv_out = T.nnet.conv.conv2d(x_t, self.W, border_mode=self.border_mode,
+                                      subsample=self.subsample)
+        outputs = self.activation(conv_out)
+        rec_error = T.sqr(inputs - outputs).sum()
+        l1_norm = (self.gamma * diff_abs(x_t)).sum()
+        cost = rec_error + l1_norm
+        x, new_accum_1, new_accum_2 = _RMSPropStep(cost, x_t, accum_1, accum_2)
+        return x, new_accum_1, new_accum_2, outputs
+
+    def get_output(self, train):
+        inputs = self.get_input(train)
+        initial_states = alloc_zeros_matrix(self.batch_size, self.stack_size,
+                                            self.code_row, self.code_col)
+        outputs, updates = theano.scan(
+            self._step,
+            sequences=[],
+            outputs_info=[initial_states, ]*3 + [None, ],
+            non_sequences=inputs,
+            n_steps=self.n_steps,
+            truncate_gradient=self.truncate_gradient)
+
+        if self.return_reconstruction:
+            return outputs[-1][-1]
+        else:
+            return outputs[0][-1]
