eaf27ba04d29cb9bbf67a3987ebd3a907eff3aae
EderSantana <eder@macbook.com>
2015-07-14 17:47:30 -0400

    Added pad_md_sequences

--- seya/utils.py
+++ seya/utils.py
@@ -1,4 +1,44 @@
-from theano import tensor
+import numpy as np
+import theano.tensor as tensor
+
+
+def pad_md_sequences(sequences, maxlen=None, dtype='int32', padding='pre', truncating='pre', value=0.):
+    '''Pad multi-dimensional sequence
+    Similar to keras.preprocesing.sequence but supports a third dimension:
+
+        Pad each sequence to the same length:
+        the length of the longuest sequence.
+
+        If maxlen is provided, any sequence longer
+        than maxlen is truncated to maxlen. Truncation happens off either the beginning (default) or
+        the end of the sequence.
+
+        Supports post-padding and pre-padding (default).
+
+    '''
+    lengths = [len(s) for s in sequences]
+    dim = sequences[0].shape[-1]  # the third dimension is assumed equal for all elements
+
+    nb_samples = len(sequences)
+    if maxlen is None:
+        maxlen = np.max(lengths)
+
+    x = (np.ones((nb_samples, maxlen, dim)) * value).astype(dtype)
+    for idx, s in enumerate(sequences):
+        if truncating == 'pre':
+            trunc = s[-maxlen:]
+        elif truncating == 'post':
+            trunc = s[:maxlen]
+        else:
+            raise ValueError("Truncating type '%s' not understood" % padding)
+
+        if padding == 'post':
+            x[idx, :len(trunc)] = trunc
+        elif padding == 'pre':
+            x[idx, -len(trunc):] = trunc
+        else:
+            raise ValueError("Padding type '%s' not understood" % padding)
+    return x
 
 
 def diff_abs(z):
