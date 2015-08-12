5554bc2bc69b28c310ea868f672e30be9657dc1f
EderSantana <eder@macbook.com>
2015-08-12 11:39:48 -0400

    fixed objective

--- seya/objectives.py
+++ seya/objectives.py
@@ -6,4 +6,4 @@ def gaussianKL(dumb, y_pred):
     mean = y_pred[:, :dim]
     logsigma = y_pred[:, dim:]
     kl = -.5 - logsigma + .5 * (mean**2 + T.exp(2 * logsigma))
-    return kl + 0 * dumb.sum()
+    return kl.mean(axis=-1) + 0 * dumb.sum()
