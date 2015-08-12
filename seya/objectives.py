98e5da2b6a798630dbb55d0a8c9feeba610d3dcd
EderSantana <eder@macbook.com>
2015-08-12 11:39:10 -0400

    fixed objective

--- seya/objectives.py
+++ seya/objectives.py
@@ -1,9 +1,9 @@
 import theano.tensor as T
 
 
-def gaussianKL(y_true, y_pred):
+def gaussianKL(dumb, y_pred):
     dim = y_pred.shape[-1] / 2
     mean = y_pred[:, :dim]
     logsigma = y_pred[:, dim:]
     kl = -.5 - logsigma + .5 * (mean**2 + T.exp(2 * logsigma))
-    return kl + 0 * y_true.sum()
+    return kl + 0 * dumb.sum()
