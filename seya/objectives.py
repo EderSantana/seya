46be6cc3283e081317bc9370c880650ad03031a8
EderSantana <eder@macbook.com>
2015-08-12 11:14:28 -0400

    added guassian_prior cost

---
+++ seya/objectives.py
@@ -0,0 +1,9 @@
+import theano.tensor as T
+
+
+def gaussianKL(y_true, y_pred):
+    dim = y_pred.shape[-1] / 2
+    mean = y_pred[:, :dim]
+    logsigma = y_pred[:, dim:]
+    kl = -.5 - logsigma + .5 * (mean**2 + T.exp(2 * logsigma))
+    return kl
