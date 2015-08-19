3b4d7bd3a932400016aed3aea61b3b39afccd8f0
EderSantana <eder@macbook.com>
2015-08-19 07:01:56 -0400

    Use mean instead of sum at Expectation step

--- seya/layers/coding.py
+++ seya/layers/coding.py
@@ -221,7 +221,8 @@ class Sparse2L(Layer):
         causes = (1 + T.exp(-T.dot(u_tm1, self.V))) * .5
         l1_norm = (self.gamma * causes * diff_abs(x_tm1)).mean()
         l1_inov = diff_abs(x_tm1 - prior).mean() * self.gamma / 10.
-        cost = rec_error + l1_norm + l1_inov
+        causes_norm = diff_abs(u_tm1).mean() * self.gamma
+        cost = rec_error + l1_norm + l1_inov + causes_norm
         x, new_accum_1, new_accum_2 = _RMSPropStep(cost, x_tm1, accum_1,
                                                    accum_2)
         u, new_accum_1_u, new_accum_2_u = _RMSPropStep(cost, u_tm1, accum_1_u,
@@ -268,6 +269,86 @@ class ConvSparseCoding(Layer):
                  init='glorot_uniform', activation='linear', weights=None,
                  border_mode='valid', subsample=(1, 1),
                  W_regularizer=l2(.01), activity_regularizer=None,
+                 return_reconstruction=False, n_steps=10, truncate_gradient=-1,
+                 gamma=0.1):
+
+        super(ConvSparseCoding, self).__init__()
+        self.init = initializations.get(init)
+        self.activation = activations.get(activation)
+        self.subsample = subsample
+        self.border_mode = border_mode
+        self.nb_filter = nb_filter
+        self.stack_size = stack_size
+        self.n_steps = n_steps
+        self.truncate_gradient = truncate_gradient
+        self.gamma = gamma
+
+        self.nb_row = nb_row
+        self.nb_col = nb_col
+        if border_mode == 'valid':
+            self.code_row = input_row + nb_row - 1
+            self.code_col = input_col + nb_col - 1
+        else:
+            raise ValueError("boder_model {0} not implemented yet.".format(
+                border_mode))
+
+        self.input = T.tensor4()
+        self.W_shape = (nb_filter, stack_size, nb_row, nb_col)
+        self.W = self.init(self.W_shape)
+
+        self.params = [self.W]
+
+        self.regularizers = []
+        if W_regularizer:
+            W_regularizer.set_param(self.W)
+            self.regularizers.append(W_regularizer)
+
+        if weights is not None:
+            self.set_weights(weights)
+
+        self.return_reconstruction = return_reconstruction
+
+    def get_initial_states(self, X):
+        return alloc_zeros_matrix(X.shape[0], self.stack_size,
+                                  self.code_row, self.code_col)
+
+    def _step(self, x_t, accum_1, accum_2, inputs):
+        conv_out = T.nnet.conv.conv2d(x_t, self.W, border_mode=self.border_mode,
+                                      subsample=self.subsample)
+        outputs = self.activation(conv_out)
+        rec_error = T.sqr(inputs - outputs).sum()
+        l1_norm = (self.gamma * diff_abs(x_t)).sum()
+        cost = rec_error + l1_norm
+        x, new_accum_1, new_accum_2 = _RMSPropStep(cost, x_t, accum_1, accum_2)
+        return x, new_accum_1, new_accum_2, outputs
+
+    def _get_output(self, inputs, train):
+        initial_states = self.get_initial_states(inputs)
+        outputs, updates = theano.scan(
+            self._step,
+            sequences=[],
+            outputs_info=[initial_states, ]*3 + [None, ],
+            non_sequences=inputs,
+            n_steps=self.n_steps,
+            truncate_gradient=self.truncate_gradient)
+
+        if self.return_reconstruction:
+            return outputs[-1][-1]
+        else:
+            return outputs[0][-1]
+
+    def get_output(self, train):
+        inputs = self.get_input(train)
+        return self._get_output(inputs, train)
+
+
+class ConvSparse2L(Layer):
+    def __init__(self, nb_filter, stack_size, nb_row, nb_col,
+                 input_row, input_col,
+                 init='glorot_uniform', activation='linear', weights=None,
+                 border_mode='valid', subsample=(1, 1),
+                 W_regularizer=l2(.001), V_regularizer=l2(.001),
+                 activity_regularizer=None,
                  return_reconstruction=False, n_steps=10, truncate_gradient=-1,
                  gamma=0.1):
 
