ea2f38a028f16c96553f1d23619ddb293e503d87
EderSantana <eder@macbook.com>
2015-08-21 15:21:59 -0400

    Add ExoGRUM that gets mem as input

--- seya/layers/recurrent.py
+++ seya/layers/recurrent.py
@@ -159,3 +159,62 @@ class GRUM(GRU):
                 "inner_activation": self.inner_activation.__name__,
                 "truncate_gradient": self.truncate_gradient,
                 "return_sequences": self.return_sequences}
+
+
+class ExoGRUM(GRUM):
+    def __init__(self, input_dim, output_dim=128, mem=None,
+                 mem_dim=128, init='glorot_uniform', inner_init='orthogonal',
+                 activation='sigmoid', inner_activation='hard_sigmoid',
+                 weights=None, truncate_gradient=-1, return_sequences=False):
+
+        super(ExoGRUM, self).__init__(
+            input_dim=input_dim, output_dim=output_dim, mem=mem,
+            mem_dim=mem_dim, init=init, inner_init=inner_init,
+            activation=activation, inner_activation=inner_activation,
+            weights=weights, truncate_gradient=truncate_gradient,
+            return_sequences=return_sequences)
+
+    def get_output(self, train=False, mem_updates=False):
+        inp = self.get_input(train)
+        X = inp[:-1, :-1, :-self.mem_dim]
+        mem = inp[-1., -1:, self.mem_dim:]
+        padded_mask = self.get_padded_shuffled_mask(train, X, pad=1)
+        X = X.dimshuffle((1, 0, 2))
+
+        x_z = T.dot(X, self.W_z) + self.b_z
+        x_r = T.dot(X, self.W_r) + self.b_r
+        x_h = T.dot(X, self.W_h) + self.b_h
+        xm_z = T.dot(X, self.Wm_z) + self.bm_z
+        xm_r = T.dot(X, self.Wm_r) + self.bm_r
+        xm_h = T.dot(X, self.Wm_h) + self.bm_h
+        outputs, updates = theano.scan(
+            self._step,
+            sequences=[x_z, x_r, x_h, padded_mask, xm_z.mean(axis=0),
+                       xm_r.mean(axis=0), xm_h.mean(axis=0)],
+            outputs_info=[T.unbroadcast(alloc_zeros_matrix(X.shape[1],
+                                                           self.output_dim), 1),
+                          mem],
+            non_sequences=[self.U_z, self.U_r, self.U_h, self.Hm_z, self.Hm_r,
+                           self.Hm_h, self.Vm_z, self.Vm_r, self.Vm_h,
+                           self.Um_z, self.Um_r, self.Um_h],
+            truncate_gradient=self.truncate_gradient)
+
+        self.mem_updates = outputs[1][-1]
+
+        if mem_updates:
+            return outputs[1][-1:]
+        if self.return_sequences:
+            return outputs[0].dimshuffle((1, 0, 2))
+        return outputs[0][-1]
+
+    def get_config(self):
+        return {"name": self.__class__.__name__,
+                "input_dim": self.input_dim,
+                "output_dim": self.output_dim,
+                "mem_dim": self.mem_dim,
+                "init": self.init.__name__,
+                "inner_init": self.inner_init.__name__,
+                "activation": self.activation.__name__,
+                "inner_activation": self.inner_activation.__name__,
+                "truncate_gradient": self.truncate_gradient,
+                "return_sequences": self.return_sequences}
