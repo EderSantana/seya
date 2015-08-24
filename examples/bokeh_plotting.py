772650f5ae451f69cc2d7183336868724a63eca8
EderSantana <eder@macbook.com>
2015-08-24 14:15:23 -0400

    Add BokehPlot Callback demo

---
+++ examples/bokeh_plotting.py
@@ -0,0 +1,74 @@
+from __future__ import absolute_import
+from __future__ import print_function
+import numpy as np
+np.random.seed(1337)  # for reproducibility
+
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.optimizers import SGD, Adam, RMSprop
+from keras.utils import np_utils
+
+from seya.bokeh_callbacks import BokehPlotting
+
+'''
+    Train a simple deep NN on the MNIST dataset.
+    Get to 98.30% test accuracy after 20 epochs (there is *a lot* of margin for parameter tuning).
+    2 seconds per epoch on a GRID K520 GPU.
+
+    In order to use BokehPlotting, you have to have a server loaded up. For
+    example, run on the terimnal:
+
+    >> bokeh-server
+
+    This fire up a server at http://localhost:5006/. You can pass an ip value to
+    to bokeh-server to listen to as well:
+
+    >> bokeh-server --ip 0.0.0.0.
+
+    This will allow you to see your progress from a different machine.
+'''
+
+bokeh_cbk = BokehPlotting(name='keras_example', fig_name='MNIST MLP example',
+                          url='default')
+
+batch_size = 100
+nb_classes = 10
+nb_epoch = 20
+
+# the data, shuffled and split between tran and test sets
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+X_train = X_train.reshape(60000, 784)
+X_test = X_test.reshape(10000, 784)
+X_train = X_train.astype("float32")
+X_test = X_test.astype("float32")
+X_train /= 255
+X_test /= 255
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+model = Sequential()
+model.add(Dense(784, 128))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(Dense(128, 128))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(Dense(128, 10))
+model.add(Activation('softmax'))
+
+rms = RMSprop()
+model.compile(loss='categorical_crossentropy', optimizer=rms)
+
+model.fit(X_train, Y_train, batch_size=batch_size,
+          nb_epoch=nb_epoch, show_accuracy=True,verbose=2,
+          validation_data=(X_test, Y_test), callbacks=[bokeh_cbk])
+
+score = model.evaluate(X_test, Y_test, show_accuracy=True, verbose=0)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
