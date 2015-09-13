3129819c7d2ff3b35dd0270c0a27ef694a7e4d9e
EderSantana <eder@macbook.com>
2015-09-13 13:53:09 -0400

    Add regularizers.py
    This may be a better way to handle loss that does ask for a desired
    y_true. Add GaussianKL and SimpleCost (a refactoring of simple_cost)

---
+++ seya/regularizers.py
@@ -0,0 +1,32 @@
+from keras.regularizers import Regularizer
+
+
+class GaussianKL(Regularizer):
+    def set_param(self, p):
+        self.p = p
+
+    def set_layer(self, layer):
+        self.layer = layer
+
+    def __call__(self, loss):
+        # See Variational Auto-Encoding Bayes by Kingma and Welling.
+        mean, sigma = self.layer.get_output(True)
+        kl = -.5 - self.logsigma + .5 * (mean**2
+                                         + T.exp(2 * self.logsigma))
+        loss += kl.mean()
+        return loss
+
+    def get_config(self):
+        return {"name": self.__class__.__name__}
+
+
+class SimpleCost(Regularizer):
+    def set_param(self, cost):
+        self.cost = cost
+
+    def __call__(self, loss):
+        loss += self.cost
+        return loss
+
+    def get_config(self):
+        return {"name": self.__class__.__name__}
