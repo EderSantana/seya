e2f28092c3cd3702d1dc929eb85a79b453d3b1b3
EderSantana <eder@macbook.com>
2015-09-14 13:42:11 -0400

    Fix draw.py
    I had a temporal problem in mind, but the input x there should be fixed

--- seya/layers/draw.py
+++ seya/layers/draw.py
@@ -1,6 +1,8 @@
 import theano.tensor as T
+from theano import scan
 
 from keras.layers.recurrent import GRU, Recurrent
+from keras.utils.theano_utils import alloc_zeros_matrix
 
 from ..utils import theano_random
 from ..regularizers import SimpleCost
@@ -37,12 +39,15 @@ class DRAW(Recurrent):
 
         self.inner_rnn = inner_rnn
         if inner_rnn == 'gru':
-            self.enc = GRU(input_dim=N**2+dim, output_dim=dim)
+            self.enc = GRU(input_dim=self.N_enc**2+dim, output_dim=dim)
             self.dec = GRU(input_dim=dim, output_dim=dim)
         else:
             raise ValueError('This type of rnn is not supported')
 
-        self.L = self.enc.init((dim, 5))  # attention parameters (eq. 21)
+        self.L_enc = self.enc.init((dim, 5))  # attention parameters (eq. 21)
+        self.L_dec = self.enc.init((dim, 5))  # attention parameters (eq. 21)
+        self.params = self.enc.params + self.dec.params + [self.L_enc,
+                                                           self.L_dec]
 
     def _get_attention_params(self, h_dec, L, N):
         p = T.dot(h_dec, L)
@@ -98,14 +103,15 @@ class DRAW(Recurrent):
 
     def _get_rnn_state(self, rnn, *args):
         if self.inner_rnn == 'gru':
-            x_z, x_r, x_h, mask, h_tm1 = args
-            h = rnn._step(x_z, x_r, x_h, mask, h_tm1,
+            x_z, x_r, x_h, h_tm1 = args
+            h = rnn._step(x_z, x_r, x_h, h_tm1,
                           rnn.U_z, rnn.U_r, rnn.U_h)
         return h
 
-    def _step(self, x, mask, canvas, h_enc, h_dec, *args):
+    def _step(self, canvas, h_enc, h_dec, x, *args):
         x_hat = x - canvas
-        gx, gy, sigma2, delta, gamma = self._get_attention_params(h_dec, self.L,
+        gx, gy, sigma2, delta, gamma = self._get_attention_params(h_dec,
+                                                                  self.L_enc,
                                                                   self.N_enc)
         Fx, Fy = self._get_filterbank(gx, gy, sigma2, delta, self.N_enc)
         read_x = self._read(x, gamma, Fx, Fy).flatten(ndim=2)
@@ -114,12 +120,12 @@ class DRAW(Recurrent):
 
         x_enc_z, x_enc_r, x_enc_h = self._get_rnn_input(enc_input, self.enc)
         new_h_enc = self._get_rnn_state(self.enc, x_enc_z, x_enc_r, x_enc_h,
-                                        mask, h_enc)
+                                        h_enc)
         sample, kl = self._get_sample(new_h_enc)
 
         x_dec_z, x_dec_r, x_dec_h = self._get_rnn_input(sample, self.dec)
         new_h_dec = self._get_rnn_state(self.dec, x_dec_z, x_dec_r, x_dec_h,
-                                        mask, h_dec)
+                                        h_dec)
 
         gx_w, gy_w, sigma2_w, delta_w, gamma_w = self._get_attention_params(
             h_dec, self.L_dec, self.N_dec)
@@ -129,21 +135,20 @@ class DRAW(Recurrent):
         return new_canvas, new_h_enc, new_h_dec, kl
 
     def _get_initial_states(self, X):
-        canvas = alloc_zeros_matrix(X.shape[2:])
-        init_enc = alloc_zeros_matrix(X.shape[1], self.output_dim)
-        init_dec = alloc_zeros_matrix(X.shape[1], self.output_dim)
+        canvas = alloc_zeros_matrix(X.shape)
+        init_enc = alloc_zeros_matrix(X.shape[0], self.output_dim)
+        init_dec = alloc_zeros_matrix(X.shape[0], self.output_dim)
         return canvas, init_enc, init_dec
 
     def get_output(self, train=False):
         X = self.get_input(train)
-        padded_mask = self.get_padded_shuffled_mask(train, X, pad=1)
-        X = X.dimshuffle((1, 0, 2, 3, 4))
-        init_enc, init_dec = self._get_initial_states(X)
+        canvas, init_enc, init_dec = self._get_initial_states(X)
 
         outputs, updates = scan(self._step,
-                                sequences=[X, padded_mask],
+                                sequences=[],
                                 output_info=[canvas, init_enc, init_dec],
-                                non_sequences=self.params,
+                                non_sequences=[X, ] + self.params,
+                                n_steps=self.n_steps,
                                 truncate_gradient=self.truncate_gradient)
         self.updates = updates
         kl = outputs[-1].sum()
