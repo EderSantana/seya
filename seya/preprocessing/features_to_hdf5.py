bd70d790064bcba55864f602ad4d9fd68f97b49b
EderSantana <eder@macbook.com>
2015-11-14 15:46:19 -0500

    Update features_to_hdf5.py
    Now it has progressbar, infers the shape of the feature vector and expects for
    the dimensions to resize the input image as a parameter.

--- seya/preprocessing/features_to_hdf5.py
+++ seya/preprocessing/features_to_hdf5.py
@@ -1,27 +1,35 @@
+import os
+import numpy as np
+import h5py
+import random
+
 from PIL import Image
 from glob import glob
 from sklearn_theano.feature_extraction import OverfeatTransformer as fext
-import h5py
-from natsort import natsorted
-import os
-import numpy as np
+
+from keras.utils.generic_utils import Progbar
 
 
-def image2features(input_path, output_path='features.hdf5',
-                   feature_extractor=fext, label_callback=None):
-    ''' Extract features using a pretrained model and store to hdf5
+def features_to_hdf5(input_path, output_path='features.hdf5',
+                     feature_extractor=fext, label_callback=None,
+                     img_size=(231, 231)):
+    ''' Extract features using a pretrained model and store the results to hdf5.
 
     Parameteres:
     ------------
     input_path: str, path to images with appropriate extensions example
                 "./path/*.jpg" or "./path/*.png"
 
-    output_path: str, path to the resulting hdf5 file
+    output_path: str, path to the resulting hdf5 file. We also create a text
+                 file with the name and order of the files saved to hdf5.
 
     label_callback: optional function, a callback to get label from each file
                     name
 
     feature_extractor: sklearn-theano transformer, default: OverfeatTransformer
+
+    img_size: list (int, int): number of rows and columns for the converted
+              image
 
     Notes:
     ------
@@ -30,42 +38,60 @@ def image2features(input_path, output_path='features.hdf5',
 
     This script creates a temporary file called tttemp.hdf5 and deletes
     afterward.
+
+    This code converts one image at a time, for
+    really large datasets you may want to modify it to convert images in batches.
     '''
-    files = natsorted(glob(input_path))
+    files = glob(input_path)
+    random.shuffle(files)
+    output_txt = file(output_path + '.txt', 'w')
     tf = fext(output_layers=(-3,))
 
     count = 0
 
     with h5py.File('tttemp.hdf5', 'w') as h:
-        X = h.create_dataset('features', (len(files), 3072), dtype='f')
+        feature_size = _load_and_convert(files[0], tf, img_size).shape[-1]
+        X = h.create_dataset('features', (len(files), feature_size), dtype='f')
         if label_callback is not None:
             label_size = len(label_callback('test'))
             y = h.create_dataset('labels', (len(files), label_size), dtype='f')
 
         print("Reading a total of {} files...".format(len(files)))
+        progbar = Progbar(len(files))
         for f in files:
             try:
                 # Some of these files are not reading right...
-                im = Image.open(f, 'r')
-                im
-                I = np.array(im.resize((231, 231)))
-                im2 = tf.transform(I[np.newaxis])[0]
+                im2 = _load_and_convert(f, tf, img_size)
                 X[count] = im2
                 if label_callback is not None:
                     y[count] = label_callback(f)
                 count += 1
-                print("Converted: {}".format(f))
+                output_txt.write(f + "\n")
+                if count % 100 == 0:
+                    progbar.add(100)
+
             except:
-                print("Problem loading: {}".format(f))
+                print("Problem converting: {}".format(f))
                 continue
 
         print("Successfully transformed {} files".format(count))
         with h5py.File(output_path, 'w') as g:
-            XX = g.create_dataset('features', (count, 3072), dtype='f')
+            XX = g.create_dataset('features', (count, feature_size), dtype='f')
             if label_callback is not None:
                 yy = g.create_dataset('labels', (count, label_size), dtype='f')
                 yy[:] = y[:count]
+
+            progbar = Progbar(count)
+            print("Clearning up...")
             for i in range(count):
+                progbar.add(1)
                 XX[i] = X[i]
     del tf
+    output_txt.close()
     os.system('rm tttemp.hdf5')
+
+
+def _load_and_convert(f, tf, img_size):
+    im = Image.open(f, 'r')
+    I = np.array(im.resize(img_size))
+    return tf.transform(I[np.newaxis])[0]
