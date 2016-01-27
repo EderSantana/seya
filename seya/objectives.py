Fixes on objects, callbacks and normalization

seya/objectives.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from keras import backend as K
 
 
@@ -6,6 +8,7 @@ def sum_mse(y_true, y_pred):
 
 
 def self_cost(y_true, y_pred):
+    # output itself is cost and must be minimized
     return K.sum(y_pred) + K.sum(y_true)*0
 
 
@@ -21,5 +24,32 @@ def gaussianKL(dumb, y_pred):
 
 def correntropy(sigma=1.):
     def func(y_true, y_pred):
-        return K.sum(K.exp(-K.sqr(y_true - y_pred)/sigma))
+        return -K.sum(K.exp(-K.sqr(y_true - y_pred)/sigma))
+    return func
+
+
+def _get_kernel(X, Z, ksize):
+    G = K.sum((K.expand_dims(X, dim=1) - K.expand_dims(Z, dim=0))**2, axis=-1)  # Gram matrix
+    G = K.exp(-G/(ksize)) / K.sqrt(2*np.pi*ksize)
+    return G
+
+
+def ITLeuclidean(ksize=1.):
+    def func(y_true, y_pred):
+        Gxx = _get_kernel(y_true, y_true, ksize)
+        Gzz = _get_kernel(y_pred, y_pred, ksize)
+        Gxz = _get_kernel(y_true, y_pred, ksize)
+        cost = K.mean(Gxx) + K.mean(Gzz) - 2*K.mean(Gxz)
+        return cost
+    return func
+
+
+def ITLcsd(ksize=1.):
+    def func(y_true, y_pred):
+        Gxx = _get_kernel(y_true, y_true)
+        Gzz = _get_kernel(y_pred, y_pred)
+        Gxz = _get_kernel(y_true, y_pred)
+        cost = K.log(K.sqrt(K.mean(Gxx)*K.mean(Gzz)) /
+                     K.mean(Gxz))
+        return cost
     return func

seya/regularizers.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from keras import backend as K
 from keras.regularizers import Regularizer
 
@@ -94,4 +96,4 @@ class CorrentropyWeightRegularizer(Regularizer):
 
 
 def correntropy(x, sigma):
-    return -K.sum(K.mean(K.exp(x**2/sigma), axis=0))
+    return -K.sum(K.mean(K.exp(x**2/sigma), axis=0)) / K.sqrt(2*np.pi*sigma)